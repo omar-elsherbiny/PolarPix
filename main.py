ce2890d7fe9a494df8e224d83c87554573bfc325
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-08-08 16:10:48 +0300

    Adding Frame + updates

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import json
 import exiv2
+from PIL import Image, ImageDraw, ImageFont
 
+filename = "img.jpg"
+output_file = "res.jpg"
+
+# Loading Config
 config = {}
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
 
-filename = "img.jpg"
-image = exiv2.ImageFactory.open(filename)
+# Extracting MetaData
 exiv2.enableBMFF()
-image.readMetadata()
-data = image.exifData()
-for key in data:
-    print(key)
+exiv_image = exiv2.ImageFactory.open(filename)
+exiv_image.readMetadata()
+metadata = exiv_image.exifData()
+
+# Adding Frame
+pil_image = Image.open(filename)
+
+width, height = [int(dim * config["resize_factor"]) for dim in pil_image.size]
+framed_width = width + 2 * config["side_padding"]
+framed_height = height + config["top_padding"] + config["bottom_padding"]
+
+framed_image = Image.new(
+    pil_image.mode, (framed_width, framed_height), tuple(config["frame_color"])
+)
+framed_image.paste(
+    pil_image.resize((width, height), Image.LANCZOS),
+    (config["side_padding"], config["top_padding"]),
+)
+
+framed_image.show()
